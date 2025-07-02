cae3543cbf6792b3d7674e254318366d874b08bb
sachinkumar2245 <sachinkumm3570@gmail.com>
2025-07-02 10:18:07 +0530

    creating files

---
+++ src/main/java/com/structure/httpserver/HttpServer.java
@@ -0,0 +1,13 @@
+package com.structure.httpserver;
+/*
+*
+*
+* Driver class for http servers
+*
+*
+* */
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Server is running....");
+    }
+}
